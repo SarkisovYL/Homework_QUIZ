0a5cd550334b3b95925f791a2ad148fabb2e4a0f
Sarkisov Yuri <sarkisov.yl@yandex.ru>
2023-02-21 16:03:45 +0300

    android:launchMode="singleInstance" for PlayActivity

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.quiz
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.quiz.databinding.ActivityMainBinding
 
