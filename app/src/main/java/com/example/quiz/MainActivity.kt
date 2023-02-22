8dc279f20b7960b06ac636147bff553342e3ea85
Sarkisov Yuri <sarkisov.yl@yandex.ru>
2023-02-22 10:57:38 +0300

    correct Import

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.quiz
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.quiz.databinding.ActivityMainBinding
 
